8c5d9c82a46489a634eb59bebb57293767fa1179
maxbbkCmd <maxbbk.cmd@mail.ru>
2024-12-03 16:45:35 +0300

    Add car started function

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const btn = document.querySelector('.btn');
-const log = () => {
-  // console.log('Button is working!')
+const statusMessage = document.querySelector('.status-message')
+
+// Run you car!
+const carRun = () => {
   let startingNumber = Math.random();
   if ( startingNumber > 0.5) {
-    console.log('Car is started!')
+    carStart();
   } else {
-    setTimeout(function() {
-      console.log('Br-br-br #1')
-    }, 400)
-    setTimeout(function() {
-      console.log('Br-br-br #2')
-    }, 800)
-    setTimeout(function() {
-      console.log('Br-br-br #3')
-    }, 1200)
-    setTimeout(function() {
-      console.log('In car something wrong!!!')
-    }, 1600)
-    
+    carBroke()
   }
 }
 
-btn.addEventListener('click', log)
+// Car is stardet!
+function carStart() {
+  drawStatus(' Car is started!');
+};
+
+// Whith car something wrong!
+function carBroke() {
+  const statusNotification = [
+    ' Br #1',
+    ' Br #2',
+    ' Br #3',
+    ' Something wrong whith you car!!!'
+  ];
+  statusNotification.forEach((item, index) => {
+    setTimeout(() => {
+      drawStatus(item)
+    }, (index + 1) * 600)
+  })
+};
+
+// Log car status
+function drawStatus(status) {
+  statusMessage.innerHTML = status;
+};
+
+btn.addEventListener('click', carRun)
